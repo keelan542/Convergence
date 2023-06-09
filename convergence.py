4cb7f83884f749fd378ed59a74c5be035f748678
keelan542 <keelanb542@gmail.com>
2023-06-09 15:13:03 +0100

    Create energies list to store SCF energy of each step

--- convergence.py
+++ convergence.py
@@ -1,5 +1,11 @@
+# Define list to store energy values
+energies = []
+
 # Open file and read line by line
 with open("H_HMDS.log") as log_file:
     for line in log_file:
         if 'SCF Done' in line:
-            print(line.split()[4])
+            energies.append(float(line.split()[4]))
+
+for step, energy in enumerate(energies):
+    print('Step ' + str(step+1) + ' energy: ' + str(energy))
