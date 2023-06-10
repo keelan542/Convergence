7bcd276b273a4952fbdd84be36f940bc42a2c67f
keelan542 <keelanb542@gmail.com>
2023-06-10 18:01:44 +0100

    Made all energies relative to lowest value, and converted to kcal/mol

--- convergence.py
+++ convergence.py
@@ -10,6 +10,13 @@ with open("H_HMDS.log") as log_file:
         if 'SCF Done' in line:
             energies.append(float(line.split()[4]))
 
+# Getting zero point of energies
+zero_point = min(energies)
+
+# Redefining all items in energies list to be relative to zero_point and in kcal/mol
+for i in range(len(energies)):
+    energies[i] = (energies[i] - zero_point) * 627.51
+
 # Define list to store number of steps for plotting purposes
 steps = list(range(1, len(energies) + 1))
 
@@ -18,6 +25,6 @@ fig, ax = plt.subplots()
 ax.plot(steps, energies, marker='x')
 ax.set_xticks(steps)
 ax.set_xlabel('Step')
-ax.set_ylabel('Energy')
+ax.set_ylabel('Energy [kcal/mol]')
 ax.set_title('Convergence')
 plt.show()
