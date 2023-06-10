493f2c8d805327dbd70d6bb70019d3106d34288a
keelan542 <keelanb542@gmail.com>
2023-06-10 18:11:11 +0100

    Add passing of output files via command line, with error handling

--- convergence.py
+++ convergence.py
@@ -1,14 +1,19 @@
 # Import statements
 import matplotlib.pyplot as plt
+import sys
 
 # Define list to store energy values
 energies = []
 
 # Open file and read line by line
-with open("H_HMDS.log") as log_file:
-    for line in log_file:
-        if 'SCF Done' in line:
-            energies.append(float(line.split()[4]))
+try:
+    with open(sys.argv[1]) as log_file:
+        for line in log_file:
+            if 'SCF Done' in line:
+                energies.append(float(line.split()[4]))
+except IndexError:
+    print("No output file given!")
+    sys.exit()
 
 # Getting zero point of energies
 zero_point = min(energies)
