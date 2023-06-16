79339524091f0f95a3fd20f008819333b457574b
keelan542 <keelanb542@gmail.com>
2023-06-16 11:08:48 +0100

    Reduced frequency of x-tick marks using numpy array

--- convergence.py
+++ convergence.py
@@ -1,5 +1,6 @@
 # Import statements
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 
 # Define list to store energy values
@@ -27,7 +28,7 @@ steps = list(range(1, len(energies) + 1))
 
 # Creating plot
 plt.plot(steps, energies, marker='x')
-plt.xticks(steps)
+plt.xticks(np.arange(0, len(energies), 5))
 plt.xlabel('Step')
 plt.ylabel('Energy [kcal/mol]')
 plt.title('Convergence')
